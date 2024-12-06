d3bc5979c2eaf74f8658cff14a67dfacacad3df8
Talha Zahid <talha@foomotion.io>
2024-12-06 15:30:27 +0500

    Edit article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,40 @@
-class ArticlesController <ApplicationController
+class ArticlesController < ApplicationController
+
   def show
-    # @ before var name makes it instance variable so available in view 
-  
     @article = Article.find(params[:id])
   end
-  
+
+  def index
+    @articles = Article.all
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :description))
+      flash[:notice] = "Artice was Updated successfully"
+      redirect_to @article
+    else
+      render "edit"
+    end
+  end
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :description))
+   
+    if @article.save
+      flash[:notice] = "Article was created successfully."
+      redirect_to @article
+    else
+      render "new"
+    end
+  end
+
+
+
 end
